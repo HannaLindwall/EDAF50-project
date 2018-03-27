a82368794cd2306b07ae9a25859b6a06e5c8d270
HannaLindwall <dic14hli@student.lu.se>
2018-03-27 19:30:40 +0200

    added functions for messagehandler

--- clientserver/news/messagehandler.cc
+++ clientserver/news/messagehandler.cc
@@ -1,3 +1,57 @@
-#include <string>
-#include "connection.h"
-#include "connectionclosedexception.h"
+#include "messagehandler.h"
+
+void MessageHandler::sendByte(unsigned char code){
+  conn->write(code);
+}
+void MessageHandler::sendCode(Protocol p) {
+  sendByte(static_cast<unsigned char>(p));
+}
+void MessageHandler::sendInt(int value) {
+  sendByte((value >> 24) & 0xFF);
+  sendByte((value >> 16) & 0xFF);
+  sendByte((value >> 8) & 0xFF);
+  sendByte(value & 0xFF);
+}
+void MessageHandler::sendIntParameter(int param) {
+  sendCode(Protocol::PAR_NUM);
+  sendInt(param);
+}
+void MessageHandler::sendStringParameter(string param) {
+  sendCode(Protocol::PAR_STRING);
+  sendInt(param.length());
+  for (unsigned int i = 0; i < param.length(); i++) {
+    sendByte(param[i]);
+  }
+}
+int MessageHandler::recvByte() {
+  unsigned char code = conn->read();
+  return code;
+}
+int MessageHandler::recvCode() {
+  unsigned int code = recvByte();
+  return code;
+}
+int MessageHandler::recvInt() {
+  unsigned char b1 = recvByte();
+  unsigned char b2 = recvByte();
+  unsigned char b3 = recvByte();
+  unsigned char b4 = recvByte();
+  return (b1 << 24) | (b2 << 16) | (b3 << 8) | b4;
+}
+
+int MessageHandler::recvIntParameter() {
+  unsigned int code = recvCode();
+  return recvInt();
+}
+string MessageHandler::recvStringParameter() {
+  unsigned int code = recvCode();
+
+  unsigned int n = recvInt();
+
+  string result;
+  for (unsigned int i = 1; i <= n; i++) {
+    unsigned char ch = conn->read();
+    result += ch;
+  }
+  return result;
+}
--- clientserver/news/messagehandler.h
+++ clientserver/news/messagehandler.h
@@ -1,11 +1,23 @@
 #include <string>
 #include "connection.h"
 #include "connectionclosedexception.h"
+#include "protocol.h"
+using std::string;
 
 class MessageHandler {
 public:
-  MessageHandler(Connection& c) : conn(c) {}
+  MessageHandler(Connection& c) : conn(&c) {}
+  void sendByte(unsigned char code);
+  void sendCode(Protocol p);
+  void sendInt(int value);
+  void sendIntParameter(int param);
+  void sendStringParameter(string param);
+  int recvByte();
+  int recvCode();
+  int recvInt();
+  int recvIntParameter();
+  string recvStringParameter();
 private:
-  Connection conn;
+  Connection* conn;
 
 };
