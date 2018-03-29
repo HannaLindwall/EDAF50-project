650aeb9c641b41b3ddbf2e5e7b6d1268c6eba5f0
Tobias94Olsson <dat14tol@student.lu.se>
2018-03-29 11:34:07 +0200

    Implement database1, compiling, not sure if working

--- clientserver/news/article.h
+++ clientserver/news/article.h
@@ -1,18 +1,18 @@
+#ifndef ARTICLE_H
+#define ARTICLE_H
 #include <string>
 using std::string;
 
-
 class Article{
 public:
-  Article(unsigned int id, string article_title, string article_author, string article_text);
-  unsigned int getIdNbr();
+  Article(string article_title, string article_author, string article_text);
   string getTitle();
   string getAuthor();
   string getArticleText();
 
 private:
-  unsigned int id_nbr;
   string title;
   string author;
   string text;
 };
+#endif
--- clientserver/news/database1.cc
+++ clientserver/news/database1.cc
@@ -1,17 +1,40 @@
 #include "database1.h"
+#include "article.h"
 #include <string>
 #include <vector>
 using namespace std;
 
 Database1::Database1(){}
 vector<string> Database1::listNewsGroup(){
-  return {"hej"};
+  return newsGroups;
 }
-void Database1::createNewsGroup(string news_group_name){}
-void Database1::deleteNewsGroup(unsigned int news_group_id){}
-vector<string> Database1::listArticles(unsigned int news_group_id){}
-void Database1::createArticle(unsigned int news_group_id, string title, string author, string text){}
-void Database1::deleteArticle(unsigned int news_group_id, unsigned int article_id){}
+
+void Database1::createNewsGroup(string news_group_name){ //Borde troligen returnera om vi lyckas eller ej
+  newsGroups.push_back(news_group_name);				 //Alltså, om den fanns i vectorn eller ej
+}
+
+void Database1::deleteNewsGroup(unsigned int news_group_id){
+  newsGroups.erase(newsGroups.begin()+(news_group_id-1));
+}
+
+vector<string> Database1::listArticles(unsigned int news_group_id){
+  vector<Article> v = articles[news_group_id];
+  vector<string> returnv;
+  for(Article a : v){
+  	returnv.push_back(a.getTitle());
+  }
+  return returnv;
+}
+
+void Database1::createArticle(unsigned int news_group_id, string title, string author, string text){
+  articles[news_group_id].emplace_back(title, author, text);
+}
+
+void Database1::deleteArticle(unsigned int news_group_id, unsigned int article_id){
+  articles[news_group_id].erase(articles[news_group_id].begin()+(article_id-1));
+}
+
 string Database1::getArticle(unsigned int news_group_id, unsigned int article_id){
-	return "hej";
+  vector<Article> v = articles[news_group_id];
+  return v[article_id].getArticleText();
 }
